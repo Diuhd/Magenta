e16f5c2fc363e4a297eccc647c13b639b70ad2ae
Phillip <philparks714@gmail.com>
2024-06-24 17:29:47 +0900

    Initial commit

---
+++ src/main/java/com/diuhd/magenta/EventAssigner.kt
@@ -0,0 +1,62 @@
+package com.diuhd.magenta
+
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener
+import org.bukkit.plugin.java.JavaPlugin
+
+class EventAssigner<T : Event>(private val eventClass: Class<T>, private val plugin: JavaPlugin) {
+    private var handler: (T) -> Unit = { _ -> }
+    private var cancelled: Boolean = false
+    private var remainingTriggers: Int? = null
+    private var listener: Listener? = null
+
+    init {
+        listener = object : Listener {
+            @EventHandler
+            fun onEvent(event: Event) {
+                if (eventClass.isInstance(event)) {
+                    val typedEvent = eventClass.cast(event)
+                    if (typedEvent is Cancellable && cancelled) {
+                        typedEvent.isCancelled = true
+                    }
+                    handler(typedEvent)
+
+                    remainingTriggers?.let {
+                        if (it > 1) {
+                            remainingTriggers = it - 1
+                        } else {
+                            unregister()
+                        }
+                    }
+                }
+            }
+        }
+        plugin.server.pluginManager.registerEvents(listener!!, plugin)
+    }
+
+    fun handler(func: (T) -> Unit): EventAssigner<T> {
+        handler = func
+        return this
+    }
+
+    fun cancelled(bool: Boolean): EventAssigner<T> {
+        if (Cancellable::class.java.isAssignableFrom(eventClass)) {
+            cancelled = bool
+        } else {
+            throw IllegalArgumentException("Event type ${eventClass.simpleName} cannot be cancelled")
+        }
+        return this
+    }
+
+    fun expiresIn(runtime: Int): EventAssigner<T> {
+        remainingTriggers = runtime
+        return this
+    }
+
+    private fun unregister() {
+        listener?.let { HandlerList.unregisterAll(it) }
+    }
+}
